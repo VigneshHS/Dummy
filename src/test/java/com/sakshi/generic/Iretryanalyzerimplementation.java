ceb8be8760149ff1cf0ab60a814054505ed89f08
vigne <vigne@Vignesh>
2023-01-21 19:35:04 +0530

    Hu

--- src/test/java/com/sakshi/generic/Iretryanalyzerimplementation.java
+++ src/test/java/com/sakshi/generic/Iretryanalyzerimplementation.java
@@ -3,6 +3,7 @@ package com.sakshi.generic;
 public class Iretryanalyzerimplementation {
   public static void main(String ar[]){
     System.out.println("Hi");
+    System.out.println("hlo");
   }
 
 }
